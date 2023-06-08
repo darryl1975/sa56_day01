845add75ad1b3f0439add4fc61b669792428038b
Darryl Ng <darryl1975@hotmail.com>
2023-06-08 14:58:23 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/sa56_day01/Sa56Day01Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.sa56_day01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa56Day01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa56Day01Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/sa56_day01/Sa56Day01ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.sa56_day01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa56Day01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
